7745d77db6c88913d2dcadf70748a5de873ddfc7
Roman Iakobchuk <r.iakobchuk@gmail.com>
2015-09-06 18:22:25 +0300

    client App without logic

--- app/src/client.js
+++ app/src/client.js
@@ -1,1 +1,14 @@
-console.log('---', 'hello world');
+import initStore from 'redux/store'
+import React from 'react'
+import routes from 'router'
+import {Router} from 'react-router'
+import BrowserHistory from 'react-router/lib/BrowserHistory'
+import {Provider} from 'react-redux'
+
+const initialState = window.__initialState || {}
+const history = new BrowserHistory
+React.render((
+    <Provider store = {initStore()} >
+        {() => <Router {...initialState} history = {history} children={routes}/> }
+    </Provider>
+), document.getElementById('content'));
